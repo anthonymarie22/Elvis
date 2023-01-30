7fb9c815432f2dda4f013a66b14d2bd67cfec014
anthonymarie22 <72616953+anthonymarie22@users.noreply.github.com>
2023-01-30 17:19:08 -0500

    button challenge 3

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = ""
+    @State private var imageString = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+            
+            Text("What's So Funny 'Bout")
+                .font(.largeTitle)
+                .foregroundColor(.purple)
+                .fontWeight(.black)
+            Spacer()
+            Image(systemName: imageString)
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.black)
+            Text(messageString)
+                .font(.largeTitle)
+                .foregroundColor(.purple)
+                .fontWeight(.black)
+            HStack{
+                Button("Peace") {
+                    messageString = "Peace"
+                    imageString = "peacesign"
+                }
+                    Button("Love") {
+                        messageString = "Love"
+                        imageString = "heart"
+                }
+                Button("Understanding") {
+                    messageString = "Understanding"
+                    imageString = "lightbulb"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+                    }
         .padding()
     }
 }
